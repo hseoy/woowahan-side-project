9b7149840d926ca4006319c82b9337bd2fcee6fa
Yunseo Hwang <yunseo.dev@gmail.com>
2022-09-27 20:32:45 +0900

    feat: configuration & typeorm module 설정

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,29 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        url: configService.get<string>('database.url'),
+        entities: [path.join(__dirname, '/**/*.entity{.ts,.js}')],
+        logging: configService.get<boolean>('database.logging'),
+        synchronize: configService.get<boolean>('database.autoDDL'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
