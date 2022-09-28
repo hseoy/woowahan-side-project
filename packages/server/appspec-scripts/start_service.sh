b9446e66a15fc93ccf8c0c4d3f20d11d944e99b7
Yunseo Hwang <yunseo.dev@gmail.com>
2022-09-28 21:36:59 +0900

    chore: build & deploy 설정 변경

--- packages/server/appspec-scripts/start_service.sh
+++ packages/server/appspec-scripts/start_service.sh
@@ -5,11 +5,13 @@ APP_NAME="woowahan-side-project"
 MODULE_NAME="server"
 
 PID_PATH_NAME=/woowahan-side-project/woowahan-side-project-pid
+ENV_PATH=/woowahan-side-project/.env
 
 echo "Starting $APP_NAME-$MODULE_NAME ..."
 
 if [ ! -f $PID_PATH_NAME ]; then
     cd /home/ec2-user/woowahan-side-project/packages/server
+    cp $ENV_PATH ./.env
     su ec2-user -c "nohup npm run start:prod >> /woowahan-side-project/app_stdout.log 2>&1 & echo \$! > $PID_PATH_NAME"
 
     echo "$APP_NAME-$MODULE_NAME started ..."
