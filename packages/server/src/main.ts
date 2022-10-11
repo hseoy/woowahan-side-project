a29646e7c5549e815e0454d616d702e09ef590cb
Yunseo Hwang <yunseo.dev@gmail.com>
2022-10-11 20:32:54 +0900

    fix: cors 문제 해결

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,17 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { setupSwagger } from './utils/swagger';
+import { corsOrigin } from './utils/cors';
 
 async function bootstrap() {
   const port = 3001;
   const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:3000',
-        'https://woowahan-side-project.hseoy.xyz/',
-      ],
-      credentials: true,
-    },
+    cors: { origin: corsOrigin, credentials: true },
   });
 
   setupSwagger(app);
