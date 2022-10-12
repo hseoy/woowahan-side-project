56278f9025c1c38135dbf428ffe76b8ff8be857a
Yunseo Hwang <yunseo.dev@gmail.com>
2022-10-12 22:59:23 +0900

    chore: 을지로10년후체 폰트에 대해 서브셋 적용

--- packages/web-client/styles/Fonts.tsx
+++ packages/web-client/styles/Fonts.tsx
@@ -1,5 +1,6 @@
 import { Global } from '@emotion/react';
 
+// 을지로10년후체는 용량이 크기 때문에(7MB) 헤더 영역 등, 한정된 영역에서만 사용하며 서브셋으로 생성하여 사용
 function Fonts() {
   return (
     <Global
@@ -10,9 +11,12 @@ function Fonts() {
           font-weight: normal;
           font-style: normal;
         }
+        
         @font-face {
           font-family: 'BMEuljiro10yearslater';
-          src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-10-21@1.0/BMEuljiro10yearslater.woff') format('woff');
+          src: url('./BMEuljiro10yearslater/BMEuljiro10yearslater.subset.woff2') format('woff2'),
+          url('./BMEuljiro10yearslater/BMEuljiro10yearslater.subset.woff') format('woff'),
+          url('./BMEuljiro10yearslater/BMEuljiro10yearslater.subset.ttf') format('truetype');
           font-weight: normal;
           font-style: normal;
         }
