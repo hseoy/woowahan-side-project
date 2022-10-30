a91adc35769af28da360731bdaad836e9da24371
이찬호 <0407chan@naver.com>
2022-10-31 00:16:36 +0900

    [좋아요 기능] findByProjectId에 userId 받도록 수정

--- packages/server/src/projects/like/like.module.ts
+++ packages/server/src/projects/like/like.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { LikeController } from './like.controller';
 import { UsersModule } from '@/users/users.module';
-import { ProjectsModule } from '@/projects/projects.module';
+// import { ProjectsModule } from '@/projects/projects.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Like } from './entities/like.entity';
+import { Project } from '../entities/project.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Like]), UsersModule, ProjectsModule],
+  imports: [TypeOrmModule.forFeature([Like, Project]), UsersModule],
   controllers: [LikeController],
+  exports: [LikeService],
   providers: [LikeService],
 })
 export class LikeModule {}
