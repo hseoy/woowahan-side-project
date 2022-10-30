839880d820354266fadfed1f77d5f0c2649d6bf5
이찬호 <0407chan@naver.com>
2022-10-30 16:33:58 +0900

    [좋아요 기능] featrue. liks api 추가

---
+++ packages/web-client/apis/liks/dto.ts
@@ -0,0 +1,20 @@
+export type LikeDto = {
+  id: number;
+  like: LikeEnum;
+  projectId: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export enum LikeEnum {
+  BEAUTY = '예뻐요',
+  NEED = '필요했어요',
+  FEEL = '느낌있어요',
+  STARTUP = '창업하세요',
+  SHARE = '공유하고싶어요',
+  LIKE = '좋아요',
+}
+
+export type LikeListDto = LikeDto[];
+
+export type CreateLikeDto = Omit<LikeDto, 'id' | 'createdAt' | 'updatedAt'>;
