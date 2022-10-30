d9050fef5c2cb69aa706fa76c99627eec13e6fe0
이찬호 <0407chan@naver.com>
2022-10-31 01:10:25 +0900

    [툴팁] feature. 링크에 툴팁 반영

--- packages/web-client/components/project/PlatformDeployLink.tsx
+++ packages/web-client/components/project/PlatformDeployLink.tsx
@@ -1,10 +1,11 @@
-import { Flex, Icon, Link } from '@chakra-ui/react';
+import { Flex, Icon, Link, Text } from '@chakra-ui/react';
 import React from 'react';
 import IconAndroid from '@/assets/svg/icon_android.svg';
 import IconIos from '@/assets/svg/icon_ios.svg';
 import IconWeb from '@/assets/svg/icon_web.svg';
 import IconGithub from '@/assets/svg/icon_github.svg';
 import IconLink from '@/assets/svg/icon_link.svg';
+import Tooltip from '../common/Tooltip';
 
 type PlatformDeployLinkProps = {
   etcDeployLink?: string;
@@ -29,14 +30,16 @@ function PlatformDeployLink({
     <Flex gap="12px" onClick={stopPropagation}>
       {githubLink && (
         <Link href={githubLink} target="_blank" rel="noopener noreferrer">
-          <Icon
-            as={IconGithub}
-            _hover={{ color: '#1a1a1a' }}
-            transition="0.2s color ease"
-            color="iconColor"
-            width="32px"
-            height="32px"
-          />
+          <Tooltip content={<Text>Github Repo</Text>}>
+            <Icon
+              as={IconGithub}
+              _hover={{ color: '#1a1a1a' }}
+              transition="0.2s color ease"
+              color="iconColor"
+              width="32px"
+              height="32px"
+            />
+          </Tooltip>
         </Link>
       )}
 
@@ -46,53 +49,61 @@ function PlatformDeployLink({
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Icon
-            as={IconAndroid}
-            _hover={{ color: '#35a700' }}
-            transition="0.2s color ease"
-            color="iconColor"
-            width="32px"
-            height="32px"
-          />
+          <Tooltip content={<Text>안드로이드 다운로드</Text>}>
+            <Icon
+              as={IconAndroid}
+              _hover={{ color: '#35a700' }}
+              transition="0.2s color ease"
+              color="iconColor"
+              width="32px"
+              height="32px"
+            />
+          </Tooltip>
         </Link>
       )}
 
       {iosDeployLink && (
         <Link href={iosDeployLink} target="_blank" rel="noopener noreferrer">
-          <Icon
-            as={IconIos}
-            _hover={{ color: '#555555' }}
-            transition="0.2s color ease"
-            color="iconColor"
-            width="32px"
-            height="32px"
-          />
+          <Tooltip content={<Text>앱스토어 다운로드</Text>}>
+            <Icon
+              as={IconIos}
+              _hover={{ color: '#555555' }}
+              transition="0.2s color ease"
+              color="iconColor"
+              width="32px"
+              height="32px"
+            />
+          </Tooltip>
         </Link>
       )}
 
       {webDeployLink && (
         <Link href={webDeployLink} target="_blank" rel="noopener noreferrer">
-          <Icon
-            as={IconWeb}
-            _hover={{ color: '#005ed1' }}
-            transition="0.2s color ease"
-            color="iconColor"
-            width="32px"
-            height="32px"
-          />
+          <Tooltip content={<Text>웹페이지</Text>}>
+            <Icon
+              as={IconWeb}
+              _hover={{ color: '#005ed1' }}
+              transition="0.2s color ease"
+              color="iconColor"
+              width="32px"
+              height="32px"
+            />
+          </Tooltip>
         </Link>
       )}
 
       {etcDeployLink && (
         <Link href={etcDeployLink} target="_blank" rel="noopener noreferrer">
-          <Icon
-            as={IconLink}
-            _hover={{ color: 'logo' }}
-            transition="0.2s color ease"
-            color="iconColor"
-            width="32px"
-            height="32px"
-          />
+          <Tooltip content={<Text>링크 이동</Text>}>
+            <Icon
+              as={IconLink}
+              _hover={{ color: 'logo' }}
+              transition="0.2s color ease"
+              color="iconColor"
+              width="32px"
+              height="32px"
+            />
+          </Tooltip>
         </Link>
       )}
     </Flex>
